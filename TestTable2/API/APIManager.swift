feb97032069f75497e25c31ad640d8400c758890
Gleb Zadonskiy <vilomy2@gmail.com>
2023-03-09 11:49:24 +0300

    расшифровываю полученные данные помещаю их в массив постов

--- TestTable2/API/APIManager.swift
+++ TestTable2/API/APIManager.swift
@@ -12,11 +12,18 @@ class APIManager {
 	
 	let urlString = "https://jsonplaceholder.typicode.com/photos/"
 	
-	func getData() {
+	func getData(completion: @escaping ([Post]) -> Void) {
 		let url = URL(string: urlString)!
 		let request = URLRequest(url: url)
 		let task = URLSession.shared.dataTask(with: request) { data, response, error in
-			print(response)
+			guard let data else {return}
+			do {
+				let postData = try JSONDecoder().decode([Post].self, from: data)
+				completion(postData)
+			} catch {
+				print("Error decoding")
+				print(error.localizedDescription)
+			}
 		}
 		task.resume()
 	}
--- TestTable2/ViewController.swift
+++ TestTable2/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 class ViewController: UIViewController {
 
 	private var tableView = UITableView()
+	private var posts: [Post] = []
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		setupViews()
+		
+		APIManager.shared.getData { [weak self] data in
+			guard let self else {return}
+			self.posts = data
+			DispatchQueue.main.async {
+				self.tableView.reloadData()
+			}
+		}
 	}
 	
 	private func setupViews() {
@@ -29,19 +38,18 @@ class ViewController: UIViewController {
 		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
 		
 		
-		APIManager.shared.getData()
 	}
 	
 }
 
 extension ViewController: UITableViewDataSource {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		10
+		posts.count
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-		cell.textLabel?.text = "\(indexPath.row)"
+		cell.textLabel?.text = posts[indexPath.row].title
 		return cell
 	}
 }
