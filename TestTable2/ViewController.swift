18de5b3806ebcb35ccd688509fd877133d1e6528
Gleb Zadonskiy <vilomy2@gmail.com>
2023-03-09 11:09:03 +0300

    Initial Commit

---
+++ TestTable2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTable2
+//
+//  Created by user05 on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
